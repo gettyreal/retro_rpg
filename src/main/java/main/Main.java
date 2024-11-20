bb2aff115d60a3d07c39b4d88187ddac0e7cf786
gettyreal <getty.fafano@gmail.com>
2024-11-20 22:44:57 +0100

    changed name

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("RetroRPG");
+        window.setTitle("POKEMON FREED EDITION");
 
         GamePanel gamePanel = new GamePanel(); //add gamePanel alla finestra
         window.add(gamePanel);
